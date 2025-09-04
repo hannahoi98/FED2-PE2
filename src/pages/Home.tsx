06457d3fa427857035f8878b1b250e4475278b54
hannahoeidahl <hanhoi02118@stud.noroff.no>
2025-09-04 21:31:37 +0200

    Base styling and setup for venue page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
-import { Typography } from "@mui/material";
+import { Divider, Stack, Typography } from "@mui/material";
+import { COLORS } from "../theme";
 
 export default function Home() {
-  return <Typography variant="h2">All Venues</Typography>;
+  return (
+    <Stack spacing={1}>
+      <Typography
+        variant="h5"
+        align="center"
+        maxWidth="280px"
+        sx={{
+          color: COLORS.pop,
+          fontFamily: '"DM Serif Display", serif',
+        }}
+      >
+        Welcome to Holidaze - Your best choice to host or book unforgettable
+        stays.
+      </Typography>
+
+      <Typography
+        component="h2"
+        variant="h4"
+        sx={{
+          fontFamily: '"DM Serif Display", serif',
+          color: COLORS.pine,
+        }}
+      >
+        All Venues
+      </Typography>
+      <Divider
+        sx={{
+          borderColor: COLORS.mint,
+          opacity: 1,
+          borderBottomWidth: 2,
+          width: "100%",
+        }}
+      />
+    </Stack>
+  );
 }
